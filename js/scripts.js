bf3bbb1935e85897b0690147703ce3ccdfc462bb
Maciek <maciejjamroz693@gmail.com>
2017-05-29 22:18:15 +0200

    dodane comciki

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+/*Nie wiem czy o to chodziło w zadaniu czy mam 
+wysłać to 2 razy. Najpierw z komentarzem w drugiej
+lini, a później z tymi 2 komantarzami?*/
+//tuaj treść zadania
+/*W pliku scripts.js zakomentuj linię z poleceniem 
+alert dowolnym typem komentarza.
+Wyślij zmiany na zdalne repozytorium.
+Dodaj jeszcze dwa inne komentarze.
+Stwórz nowe repozytorium i wyślij tam swój projekt.
+ Pamiętaj o udostępnieniu go mentorowi na Githubie. Link do
+  projektu wstaw poniżej.*/
